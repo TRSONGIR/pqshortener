6b53161d95b52fa6842680587e66e093cebcaa06
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-10-12 03:55:02 +0330

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ header("Content-Type:application/json");
 if (isset($_GET['url']) && $_GET['url']!="") {
     $longurl = $_GET['url'];
     $shortenid = $_GET['id'];
-    $result = pg_query($con,"INSERT INTO urls (shortenid, longurl) VALUES('$shortenid','$longurl');");
+    $result = pg_query($con,"INSERT INTO urls (shortenid,longurl) VALUES('$shortenid','$longurl');");
     $result = pg_query($con,"SELECT * FROM urls WHERE longurl='$longurl';");
     if(pg_fetch_row($result)>0){
         $row = pg_fetch_array($result);
