9ec694c6359b406cffbcb45a1ac7e4bc42fd23e3
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-10-12 04:49:42 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //-----------------config--------------->
-$con = pg_connect("host=ec2-54-217-213-79.eu-west-1.compute.amazonaws.com port=5432 dbname=d9jr6ppkc3n0qo user=wbrtxkjvgbuheb password=da54034e79f63243b0fccebacdc8b7954b928432a9977635866e32094a204d6e");
+$con = pg_connect("host=ec2-18-209-187-54.compute-1.amazonaws.com port=5432 dbname=dauqocoadorn20 user=wbrtxkjvgbuheb password=da54034e79f63243b0fccebacdc8b7954b928432a9977635866e32094a204d6e");
 if(!$con) {
     die('PG Connection Error');
 }
